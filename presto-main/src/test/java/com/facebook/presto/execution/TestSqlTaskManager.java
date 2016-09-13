1bad6053e92a6ad44867b47f15a1ac04ed6bdcab
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-09-13 19:25:45 +0200

    Periodically request memory revoking if needed

--- presto-main/src/test/java/com/facebook/presto/execution/TestSqlTaskManager.java
+++ presto-main/src/test/java/com/facebook/presto/execution/TestSqlTaskManager.java
@@ -67,11 +67,13 @@ public class TestSqlTaskManager
     private final TaskExecutor taskExecutor;
     private final LocalMemoryManager localMemoryManager;
     private final LocalSpillManager localSpillManager;
+    private final MemoryRevokingScheduler memoryRevokingScheduler;
 
     public TestSqlTaskManager()
     {
         localMemoryManager = new LocalMemoryManager(new NodeMemoryConfig(), new ReservedSystemMemoryConfig());
         localSpillManager = new LocalSpillManager(new NodeSpillConfig());
+        memoryRevokingScheduler = new MemoryRevokingScheduler(localMemoryManager);
         taskExecutor = new TaskExecutor(8, 16);
         taskExecutor.start();
     }
@@ -281,6 +283,7 @@ public class TestSqlTaskManager
                 new QueryMonitor(new ObjectMapperProvider().get(), jsonCodec(StageInfo.class), new EventListenerManager(), new NodeInfo("test"), new NodeVersion("testVersion"), new QueryMonitorConfig()),
                 new NodeInfo("test"),
                 localMemoryManager,
+                memoryRevokingScheduler,
                 config,
                 new NodeMemoryConfig(),
                 localSpillManager,

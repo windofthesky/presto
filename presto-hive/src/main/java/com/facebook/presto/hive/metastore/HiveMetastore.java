af3c5476810f432d5d46fd3fa0616dfc1ea49df9
Lukasz Osipiuk <lukasz@osipiuk.net>
2016-10-10 13:44:17 +0200

    Expose column statistics via HiveMetastore

--- presto-hive/src/main/java/com/facebook/presto/hive/metastore/HiveMetastore.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/metastore/HiveMetastore.java
@@ -15,11 +15,13 @@ package com.facebook.presto.hive.metastore;
 
 import com.facebook.presto.spi.security.PrestoPrincipal;
 import com.facebook.presto.spi.security.RoleGrant;
+import org.apache.hadoop.hive.metastore.api.ColumnStatisticsObj;
 import org.apache.hadoop.hive.metastore.api.Database;
 import org.apache.hadoop.hive.metastore.api.Partition;
 import org.apache.hadoop.hive.metastore.api.Table;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.Set;
 
@@ -65,6 +67,10 @@ public interface HiveMetastore
     List<Partition> getPartitionsByNames(String databaseName, String tableName, List<String> partitionNames);
 
     Optional<Table> getTable(String databaseName, String tableName);
+
+    Optional<Set<ColumnStatisticsObj>> getTableColumnStatistics(String databaseName, String tableName, Set<String> columnNames);
+
+    Optional<Map<String, Set<ColumnStatisticsObj>>> getPartitionColumnStatistics(String databaseName, String tableName, Set<String> partitionNames, Set<String> columnNames);
 
     void createRole(String role, String grantor);
 

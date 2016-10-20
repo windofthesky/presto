42c08a9b86e03029d94d1a81c8df7deaf15b9a18
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-10-20 10:56:55 +0200

    Trigger memory revoking in OperatorAssertions

--- presto-main/src/test/java/com/facebook/presto/operator/OperatorAssertion.java
+++ presto-main/src/test/java/com/facebook/presto/operator/OperatorAssertion.java
@@ -33,6 +33,7 @@ import static com.facebook.presto.operator.PageAssertions.assertPageEquals;
 import static com.facebook.presto.testing.assertions.Assert.assertEquals;
 import static com.facebook.presto.type.TypeJsonUtils.appendToBlockBuilder;
 import static com.google.common.base.Preconditions.checkArgument;
+import static io.airlift.concurrent.MoreFutures.getFutureValue;
 import static io.airlift.testing.Assertions.assertEqualsIgnoreOrder;
 import static org.testng.Assert.assertFalse;
 import static org.testng.Assert.assertTrue;
@@ -63,6 +64,11 @@ public final class OperatorAssertion
                     operator.finish();
                     finishing = true;
                 }
+            }
+
+            if (!operator.getOperatorContext().isWaitingForRevocableMemory().isDone()) {
+                getFutureValue(operator.startMemoryRevoke());
+                operator.finishMemoryRevoke();
             }
 
             Page outputPage = operator.getOutput();

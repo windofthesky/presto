75ebf93c3301de9bc2250d304b14c94db1e2d37c
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2016-11-21 17:56:15 -0500

    Log tests executed by AbstractTestQueryFramework
    It is helpful during the OOM errors debugging in integration tests

--- presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
+++ presto-tests/src/main/java/com/facebook/presto/tests/AbstractTestQueryFramework.java
@@ -34,6 +34,7 @@ import org.intellij.lang.annotations.Language;
 import org.testng.SkipException;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
 import org.weakref.jmx.MBeanExporter;
 import org.weakref.jmx.testing.TestingMBeanServer;
 
@@ -53,6 +54,7 @@ import static java.util.Objects.requireNonNull;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
+@Listeners({TestLoggingListener.class})
 public abstract class AbstractTestQueryFramework
 {
     private QueryRunnerSupplier queryRunnerSupplier;

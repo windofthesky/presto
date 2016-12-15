ecf4ef2af647c61cac3971b37404fd9cf9bc520f
Piotr Nowojski <piotr.nowojski@teradata.com>
2016-12-15 15:25:22 +0100

    Enable join spilling in LocalExecutionPlanner

--- presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/LocalExecutionPlanner.java
@@ -1573,6 +1573,8 @@ public class LocalExecutionPlanner
             Optional<JoinFilterFunctionFactory> filterFunctionFactory = node.getFilter()
                     .map(filterExpression -> compileJoinFilterFunction(filterExpression, probeLayout, buildSource.getLayout(), context.getTypes(), context.getSession()));
 
+            boolean outer = node.getType() == RIGHT || node.getType() == FULL;
+
             HashBuilderOperatorFactory hashBuilderOperatorFactory = new HashBuilderOperatorFactory(
                     buildContext.getNextOperatorId(),
                     node.getId(),
@@ -1581,12 +1583,12 @@ public class LocalExecutionPlanner
                     buildSource.getLayout(),
                     buildChannels,
                     buildHashChannel,
-                    node.getType() == RIGHT || node.getType() == FULL,
+                    outer,
                     filterFunctionFactory,
                     10_000,
                     buildContext.getDriverInstanceCount().orElse(1),
                     pagesIndexFactory,
-                    false,
+                    spillEnabled && buildContext.getDriverInstanceCount().orElse(1) > 1 && !outer,
                     memoryLimitBeforeSpill,
                     singleStreamSpillerFactory,
                     partitioningSpillerFactory);

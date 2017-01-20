ecfb63e58e8efa76036835acfc026cd6abeca1b7
Piotr Nowojski <piotr.nowojski@teradata.com>
2017-01-20 15:57:07 +0100

    Distribute aggregation for empty grouping set
    This change is about distributing the case when there is some empty grouping set
    and some non empty (like in ROLLUP).
    
    This is achieved by moving production of a default output (when there were no input rows)
    from FINAL Aggregation, to all PARTIAL Aggregation operators. Results of
    PARTIAL aggregation will be distributed among FINAL aggregations based on sum of all
    grouping sets. For example in query:
    
    SELECT count(*) FROM nation GROUP BY GROUPING SETS ((regionkey), (nationkey), ());
    
    PARTIAL results will be distributed based on: (regionkey, nationkey, groupid).
    For (regionkey) GROUPING SET, it would be (regiokney, NULL, 1)
    For (nationkey) GROUPING SET, it would be (NULL, nationkey, 2)
    For () GROUPING SET, it would be (NULL, NULL, 3)
    
    Default output will be produced by every PARTIAL Aggregation operator
    but all rows with key (NULL, NULL, 3) will be distributed to same FINAL Aggregation
    node, where all those default output rows will be merged to single output row.

--- presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
+++ presto-main/src/main/java/com/facebook/presto/operator/HashAggregationOperator.java
@@ -28,10 +28,8 @@ import com.facebook.presto.spiller.LocalSpillContext;
 import com.facebook.presto.spiller.Spiller;
 import com.facebook.presto.spiller.SpillerFactory;
 import com.facebook.presto.sql.gen.JoinCompiler;
-import com.facebook.presto.sql.planner.optimizations.HashGenerationOptimizer;
 import com.facebook.presto.sql.planner.plan.AggregationNode.Step;
 import com.facebook.presto.sql.planner.plan.PlanNodeId;
-import com.facebook.presto.type.TypeUtils;
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -44,6 +42,8 @@ import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static com.facebook.presto.operator.aggregation.builder.InMemoryHashAggregationBuilder.toTypes;
+import static com.facebook.presto.sql.planner.optimizations.HashGenerationOptimizer.INITIAL_HASH_VALUE;
+import static com.facebook.presto.type.TypeUtils.NULL_HASH_CODE;
 import static com.google.common.base.Preconditions.checkState;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.util.Objects.requireNonNull;
@@ -446,7 +446,7 @@ public class HashAggregationOperator
                 if (!inputProcessed && produceDefaultOutput) {
                     // global aggregations always generate an output row with the default aggregation output (e.g. 0 for COUNT, NULL for SUM)
                     finished = true;
-                    return getGlobalAggregationOutput();
+                    return getGlobalAggregationOutput(step.isOutputPartial());
                 }
 
                 if (aggregationBuilder == null) {
@@ -492,7 +492,7 @@ public class HashAggregationOperator
         }
     }
 
-    private Page getGlobalAggregationOutput()
+    private Page getGlobalAggregationOutput(boolean outputPartial)
     {
         List<Accumulator> accumulators = accumulatorFactories.stream()
                 .map(AccumulatorFactory::createAccumulator)
@@ -514,11 +514,17 @@ public class HashAggregationOperator
             }
 
             if (hashChannel.isPresent()) {
-                output.getBlockBuilder(channel++).writeLong(calculateDefaultOutputHash(groupByTypes, groupIdChannel.get(), groupId));
+                long hashValue = calculateDefaultOutputHash(groupByTypes, groupIdChannel.get(), groupId);
+                output.getBlockBuilder(channel++).writeLong(hashValue);
             }
 
             for (int j = 0; j < accumulators.size(); channel++, j++) {
-                accumulators.get(j).evaluateFinal(output.getBlockBuilder(channel));
+                if (outputPartial) {
+                    accumulators.get(j).evaluateIntermediate(output.getBlockBuilder(channel));
+                }
+                else {
+                    accumulators.get(j).evaluateFinal(output.getBlockBuilder(channel));
+                }
             }
         }
 
@@ -531,10 +537,10 @@ public class HashAggregationOperator
     private static long calculateDefaultOutputHash(List<Type> groupByChannels, int groupIdChannel, int groupId)
     {
         // Default output has NULLs on all columns except of groupIdChannel
-        long result = HashGenerationOptimizer.INITIAL_HASH_VALUE;
+        long result = INITIAL_HASH_VALUE;
         for (int channel = 0; channel < groupByChannels.size(); channel++) {
             if (channel != groupIdChannel) {
-                result = CombineHashFunction.getHash(result, TypeUtils.NULL_HASH_CODE);
+                result = CombineHashFunction.getHash(result, NULL_HASH_CODE);
             }
             else {
                 result = CombineHashFunction.getHash(result, BigintType.hash(groupId));

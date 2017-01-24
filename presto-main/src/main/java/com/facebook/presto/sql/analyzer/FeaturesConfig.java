046ee3ff094d926c80d53e24c0f3ce3b21b6a587
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-01-24 14:20:04 +0100

    Add config and session property deprecated.legacy-timestamp
    This is flag for switching between legacy TIME/TIMESTAMP semantic
    and new one being aligned with ANSI SQL. See: #7122

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
@@ -56,6 +56,7 @@ public class FeaturesConfig
     private boolean exchangeCompressionEnabled = false;
     private boolean legacyArrayAgg;
     private boolean legacyOrderBy;
+    private boolean legacyTimestamp = true;
     private boolean legacyMapSubscript;
     private boolean optimizeMixedDistinctAggregations;
     private boolean distributedSort = false;
@@ -168,6 +169,18 @@ public class FeaturesConfig
     public boolean isLegacyOrderBy()
     {
         return legacyOrderBy;
+    }
+
+    @Config("deprecated.legacy-timestamp")
+    public FeaturesConfig setLegacyTimestamp(boolean value)
+    {
+        this.legacyTimestamp = value;
+        return this;
+    }
+
+    public boolean isLegacyTimestamp()
+    {
+        return legacyTimestamp;
     }
 
     @Config("deprecated.legacy-map-subscript")

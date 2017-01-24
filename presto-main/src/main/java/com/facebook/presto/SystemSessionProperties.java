3a976d9e8e146390c1c0922edbcb04204cbae7d7
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-01-24 14:20:04 +0100

    Add config and session property deprecated.legacy-timestamp
    This is flag for switching between legacy TIME/TIMESTAMP semantic
    and new one being aligned with ANSI SQL. See: #7122

--- presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
+++ presto-main/src/main/java/com/facebook/presto/SystemSessionProperties.java
@@ -70,6 +70,9 @@ public final class SystemSessionProperties
     public static final String ITERATIVE_OPTIMIZER = "iterative_optimizer_enabled";
     public static final String ITERATIVE_OPTIMIZER_TIMEOUT = "iterative_optimizer_timeout";
     public static final String EXCHANGE_COMPRESSION = "exchange_compression";
+    public static final String LEGACY_TIMESTAMP = "legacy_timestamp";
+    public static final String PUSH_AGGREGATION_THROUGH_JOIN = "push_aggregation_through_join";
+    public static final String PARSE_DECIMAL_LITERALS_AS_DOUBLE = "deprecated_parse_decimal_literals_as_double";
     public static final String ENABLE_INTERMEDIATE_AGGREGATIONS = "enable_intermediate_aggregations";
 
     private final List<PropertyMetadata<?>> sessionProperties;
@@ -303,6 +306,11 @@ public final class SystemSessionProperties
                         featuresConfig.isExchangeCompressionEnabled(),
                         false),
                 booleanSessionProperty(
+                        LEGACY_TIMESTAMP,
+                        "Use legacy TIMESTAMP, TIME & DATE semantics",
+                        featuresConfig.isLegacyTimestamp(),
+                        true),
+                booleanSessionProperty(
                         ENABLE_INTERMEDIATE_AGGREGATIONS,
                         "Enable the use of intermediate aggregations",
                         featuresConfig.isEnableIntermediateAggregations(),
@@ -461,6 +469,12 @@ public final class SystemSessionProperties
     public static boolean isNewOptimizerEnabled(Session session)
     {
         return session.getSystemProperty(ITERATIVE_OPTIMIZER, Boolean.class);
+    }
+
+    @Deprecated
+    public static boolean isLegacyTimestamp(Session session)
+    {
+        return session.getSystemProperty(LEGACY_TIMESTAMP, Boolean.class);
     }
 
     public static Duration getOptimizerTimeout(Session session)

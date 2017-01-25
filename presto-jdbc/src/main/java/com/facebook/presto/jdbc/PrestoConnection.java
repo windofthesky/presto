c795bf3fab5c93cccf43d34719aac3c7a580b73d
Eric Diven <eric.diven@teradata.com>
2017-01-25 16:06:52 -0500

    Improve properties/URL parameter handling

--- presto-jdbc/src/main/java/com/facebook/presto/jdbc/PrestoConnection.java
+++ presto-jdbc/src/main/java/com/facebook/presto/jdbc/PrestoConnection.java
@@ -74,8 +74,9 @@ public class PrestoConnection
     private final Map<String, SelectedRole> roles = new ConcurrentHashMap<>();
     private final AtomicReference<String> transactionId = new AtomicReference<>();
     private final QueryExecutor queryExecutor;
+    private final Runnable onClose;
 
-    PrestoConnection(PrestoDriverUri uri, String user, QueryExecutor queryExecutor)
+    PrestoConnection(PrestoConnectionConfig uri, String user, QueryExecutor queryExecutor, Runnable onClose)
             throws SQLException
     {
         requireNonNull(uri, "uri is null");
@@ -86,6 +87,8 @@ public class PrestoConnection
 
         this.user = requireNonNull(user, "user is null");
         this.queryExecutor = requireNonNull(queryExecutor, "queryExecutor is null");
+        this.onClose = requireNonNull(onClose, "onClose is null");
+
         timeZoneId.set(TimeZone.getDefault().getID());
         locale.set(Locale.getDefault());
     }
@@ -165,6 +168,7 @@ public class PrestoConnection
     public void close()
             throws SQLException
     {
+        onClose.run();
         closed.set(true);
     }
 

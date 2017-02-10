a16962fa6bcc099b46c896d7f83ffe8343eb1e84
Andrii Rosa <Andriy.Rosa@TERADATA.COM>
2017-02-09 20:57:45 -0500

    Implement Create/Drop/List roles in Hive connector

--- presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
+++ presto-hive/src/test/java/com/facebook/presto/hive/TestHiveIntegrationSmokeTest.java
@@ -1441,7 +1441,7 @@ public class TestHiveIntegrationSmokeTest
             throws Exception
     {
         assertUpdate("CREATE TABLE tmp_complex1 AS SELECT " +
-                "ARRAY [MAP(ARRAY['a', 'b'], ARRAY[DOUBLE '2.0', DOUBLE '4.0']), MAP(ARRAY['c', 'd'], ARRAY[DOUBLE '12.0', DOUBLE '14.0'])] AS a",
+                        "ARRAY [MAP(ARRAY['a', 'b'], ARRAY[DOUBLE '2.0', DOUBLE '4.0']), MAP(ARRAY['c', 'd'], ARRAY[DOUBLE '12.0', DOUBLE '14.0'])] AS a",
                 1);
 
         assertQuery(
@@ -1863,7 +1863,7 @@ public class TestHiveIntegrationSmokeTest
     }
 
     private static class RollbackException
-        extends RuntimeException
+            extends RuntimeException
     {
     }
 

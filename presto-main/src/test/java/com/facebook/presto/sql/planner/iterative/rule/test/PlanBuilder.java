ad0245f2511425df44229f6c4a2edff34b71dedf
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-02-16 11:50:23 +0100

    Migrate PredicatePushDown#visitMarkDistinct

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
@@ -35,6 +35,7 @@ import com.facebook.presto.sql.planner.plan.DeleteNode;
 import com.facebook.presto.sql.planner.plan.ExchangeNode;
 import com.facebook.presto.sql.planner.plan.FilterNode;
 import com.facebook.presto.sql.planner.plan.LimitNode;
+import com.facebook.presto.sql.planner.plan.MarkDistinctNode;
 import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
 import com.facebook.presto.sql.planner.plan.SampleNode;
@@ -250,6 +251,11 @@ public class PlanBuilder
     public AssignUniqueId assignUniqueId(Symbol uniqueId, PlanNode source)
     {
         return new AssignUniqueId(idAllocator.getNextId(), source, uniqueId);
+    }
+
+    public PlanNode markDistinct(Symbol marker, PlanNode source)
+    {
+        return new MarkDistinctNode(idAllocator.getNextId(), source, marker, source.getOutputSymbols(), Optional.empty());
     }
 
     public Symbol symbol(String name, Type type)

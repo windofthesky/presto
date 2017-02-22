f863ce4961f755f0d9c8928196bf9e8e28ac01b7
Maciej Grzybek <maciej.grzybek@teradata.com>
2017-02-22 13:16:52 -0500

    Add aggregation to PlanBuilder in tests

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
@@ -309,6 +309,11 @@ public class PlanBuilder
         return new JoinNode(idAllocator.getNextId(), type, left, right, criteria, outputSymbols, filter, leftHashSymbol, rightHashSymbol, Optional.empty());
     }
 
+    public AggregationNode aggregate(Map<Symbol, AggregationNode.Aggregation> assignments, List<List<Symbol>> groupingSets, PlanNode source)
+    {
+        return new AggregationNode(idAllocator.getNextId(), source, assignments, groupingSets, AggregationNode.Step.SINGLE, Optional.empty(), Optional.empty());
+    }
+
     public Symbol symbol(String name, Type type)
     {
         Symbol symbol = new Symbol(name);
@@ -337,7 +342,7 @@ public class PlanBuilder
                 0);
     }
 
-    public PlanNode aggregate(ImmutableMap<Symbol, AggregationNode.Aggregation> assignments, List<List<Symbol>> groupingSets, PlanNode source)
+    public AggregationNode aggregate(ImmutableMap<Symbol, AggregationNode.Aggregation> assignments, List<List<Symbol>> groupingSets, PlanNode source)
     {
         return new AggregationNode(idAllocator.getNextId(),
                 source,

345317e0a4a1d11fea49894b4ce20a5ec00ec555
Christina Wallin <christina.wallin@teradata.com>
2017-03-16 14:22:54 -0400

    Add Kerberos exact match to file access control

--- presto-main/src/main/java/com/facebook/presto/security/FileBasedSystemAccessControlRules.java
+++ presto-main/src/main/java/com/facebook/presto/security/FileBasedSystemAccessControlRules.java
@@ -20,19 +20,31 @@ import com.google.common.collect.ImmutableList;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Objects.requireNonNull;
+
 public class FileBasedSystemAccessControlRules
 {
     private final List<CatalogAccessControlRule> catalogRules;
+    private final KerberosPrincipalAccessControlRule kerberosPrincipalRule;
 
     @JsonCreator
     public FileBasedSystemAccessControlRules(
-            @JsonProperty("catalogs") Optional<List<CatalogAccessControlRule>> catalogRules)
+            @JsonProperty("catalogs") Optional<List<CatalogAccessControlRule>> catalogRules,
+            @JsonProperty("kerberosPrincipals") Optional<KerberosPrincipalAccessControlRule> kerberosPrincipalRule)
     {
+        requireNonNull(catalogRules, "catalogRules is null");
+        requireNonNull(kerberosPrincipalRule, "kerberosPrincipalRule is null");
         this.catalogRules = catalogRules.orElse(ImmutableList.of());
+        this.kerberosPrincipalRule = kerberosPrincipalRule.orElse(new KerberosPrincipalAccessControlRule(false));
     }
 
     public List<CatalogAccessControlRule> getCatalogRules()
     {
         return catalogRules;
+    }
+
+    public KerberosPrincipalAccessControlRule getKerberosPrincipalRule()
+    {
+        return kerberosPrincipalRule;
     }
 }

e1e25a91e488c3cd1ebbb809cba46ce4ad8beb76
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-06 18:09:08 +0200

    Identify Field-s during analysis

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/Analysis.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/Analysis.java
@@ -40,7 +40,6 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.ListMultimap;
 
 import javax.annotation.concurrent.Immutable;
@@ -48,6 +47,7 @@ import javax.annotation.concurrent.Immutable;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Deque;
+import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -55,6 +55,7 @@ import java.util.Set;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static java.util.Collections.newSetFromMap;
+import static java.util.Collections.unmodifiableSet;
 import static java.util.Objects.requireNonNull;
 
 public class Analysis
@@ -66,7 +67,7 @@ public class Analysis
     private final IdentityLinkedHashMap<Table, Query> namedQueries = new IdentityLinkedHashMap<>();
 
     private final IdentityLinkedHashMap<Node, Scope> scopes = new IdentityLinkedHashMap<>();
-    private final Set<Expression> columnReferences = newSetFromMap(new IdentityLinkedHashMap<>());
+    private final IdentityHashMap<Expression, FieldId> columnReferences = new IdentityHashMap<>();
 
     private final IdentityLinkedHashMap<QuerySpecification, List<FunctionCall>> aggregates = new IdentityLinkedHashMap<>();
     private final IdentityLinkedHashMap<OrderBy, List<Expression>> orderByAggregates = new IdentityLinkedHashMap<>();
@@ -350,9 +351,9 @@ public class Analysis
         return orderByWindowFunctions.get(query);
     }
 
-    public void addColumnReferences(Set<Expression> columnReferences)
+    public void addColumnReferences(IdentityLinkedHashMap<Expression, FieldId> columnReferences)
     {
-        this.columnReferences.addAll(columnReferences);
+        this.columnReferences.putAll(columnReferences);
     }
 
     public Scope getScope(Node node)
@@ -416,7 +417,7 @@ public class Analysis
 
     public Set<Expression> getColumnReferences()
     {
-        return ImmutableSet.copyOf(columnReferences);
+        return unmodifiableSet(columnReferences.keySet());
     }
 
     public void addTypes(IdentityLinkedHashMap<Expression, Type> types)

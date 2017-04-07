431d93f291b7917cb7da1fbb50de970060cfe9a7
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-04-08 01:03:07 +0200

    Simplify PlanBuilder.tableScan

--- presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/iterative/rule/test/PlanBuilder.java
@@ -29,6 +29,7 @@ import com.facebook.presto.sql.planner.PartitioningHandle;
 import com.facebook.presto.sql.planner.PartitioningScheme;
 import com.facebook.presto.sql.planner.PlanNodeIdAllocator;
 import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.planner.TestingColumnHandle;
 import com.facebook.presto.sql.planner.TestingTableHandle;
 import com.facebook.presto.sql.planner.plan.ApplyNode;
 import com.facebook.presto.sql.planner.plan.AssignUniqueId;
@@ -74,6 +75,7 @@ import static com.facebook.presto.sql.planner.SystemPartitioningHandle.SINGLE_DI
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.collect.ImmutableList.toImmutableList;
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toMap;
 
 public class PlanBuilder
 {
@@ -137,8 +139,13 @@ public class PlanBuilder
         return new ApplyNode(idAllocator.getNextId(), input, subquery, subqueryAssignments, correlation);
     }
 
-    public TableScanNode tableScan(List<Symbol> symbols, Map<Symbol, ColumnHandle> assignments)
+    public TableScanNode tableScan(List<Symbol> symbols)
     {
+        Map<Symbol, ColumnHandle> assignments = symbols.stream()
+                .collect(toMap(
+                        symbol -> symbol,
+                        symbol -> new TestingColumnHandle(symbol.getName())));
+
         TableHandle tableHandle = new TableHandle(
                 new ConnectorId("testConnector"),
                 new TestingTableHandle());

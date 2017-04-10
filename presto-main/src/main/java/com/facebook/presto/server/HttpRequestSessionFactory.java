3b9c1f8a619f69f6d93d141dd091bd8db12ab4e9
Amruta Gokhale <amruta.gokhale@teradata.com>
2017-04-10 12:57:13 -0400

    Add catalog access control to AccessControl interface
    Every statement that accesses catalog needs to have an
    access control check to verify whether the current session
    user has access to the catalog, now that it is possible
    to have catalog access control at the system level.

--- presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionFactory.java
+++ presto-main/src/main/java/com/facebook/presto/server/HttpRequestSessionFactory.java
@@ -157,6 +157,9 @@ final class HttpRequestSessionFactory
             SessionPropertyManager sessionPropertyManager)
     {
         accessControl.checkCanSetUser(identity.getPrincipal().orElse(null), identity.getUser());
+        if (catalog != null) {
+            accessControl.checkCanAccessCatalog(identity, catalog);
+        }
 
         SessionBuilder sessionBuilder = Session.builder(sessionPropertyManager)
                 .setQueryId(queryId)

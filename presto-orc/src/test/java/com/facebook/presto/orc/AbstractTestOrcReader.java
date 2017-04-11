0135be9ea10544aba45cb739551587aa924ba22c
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-04-11 14:14:52 +0200

    fixup! Avoid using deprecated SqlTime(stamp) constructor based on session

--- presto-orc/src/test/java/com/facebook/presto/orc/AbstractTestOrcReader.java
+++ presto-orc/src/test/java/com/facebook/presto/orc/AbstractTestOrcReader.java
@@ -45,7 +45,6 @@ import static com.facebook.presto.spi.type.DoubleType.DOUBLE;
 import static com.facebook.presto.spi.type.IntegerType.INTEGER;
 import static com.facebook.presto.spi.type.RealType.REAL;
 import static com.facebook.presto.spi.type.SmallintType.SMALLINT;
-import static com.facebook.presto.spi.type.TimeZoneKey.UTC_KEY;
 import static com.facebook.presto.spi.type.TimestampType.TIMESTAMP;
 import static com.facebook.presto.spi.type.TinyintType.TINYINT;
 import static com.facebook.presto.spi.type.VarbinaryType.VARBINARY;

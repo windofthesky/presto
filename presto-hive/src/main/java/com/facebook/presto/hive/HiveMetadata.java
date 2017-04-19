070ca38c4a4f3dbb7d8523577818a1e19a37e615
Amruta Gokhale <amruta.gokhale@teradata.com>
2017-04-19 13:15:36 -0400

    Add a configuration option to disable creating non-managed Hive table
    Add a configuration option using which one can disable creating
    a non-managed (external) table. This will prevent the current
    case where one can create an external table to query any data
    in S3.

--- presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
+++ presto-hive/src/main/java/com/facebook/presto/hive/HiveMetadata.java
@@ -178,6 +178,7 @@ public class HiveMetadata
     private final boolean respectTableFormat;
     private final boolean bucketWritingEnabled;
     private final boolean writesToNonManagedTablesEnabled;
+    private final boolean createNonManagedTableEnabled;
     private final HiveStorageFormat defaultStorageFormat;
     private final TypeTranslator typeTranslator;
     private final String prestoVersion;
@@ -193,6 +194,7 @@ public class HiveMetadata
             boolean respectTableFormat,
             boolean bucketWritingEnabled,
             boolean writesToNonManagedTablesEnabled,
+            boolean createNonManagedTableEnabled,
             HiveStorageFormat defaultStorageFormat,
             TypeManager typeManager,
             LocationService locationService,
@@ -217,6 +219,7 @@ public class HiveMetadata
         this.respectTableFormat = respectTableFormat;
         this.bucketWritingEnabled = bucketWritingEnabled;
         this.writesToNonManagedTablesEnabled = writesToNonManagedTablesEnabled;
+        this.createNonManagedTableEnabled = createNonManagedTableEnabled;
         this.defaultStorageFormat = requireNonNull(defaultStorageFormat, "defaultStorageFormat is null");
         this.typeTranslator = requireNonNull(typeTranslator, "typeTranslator is null");
         this.prestoVersion = requireNonNull(prestoVersion, "prestoVersion is null");
@@ -455,6 +458,10 @@ public class HiveMetadata
         boolean external;
         String externalLocation = getExternalLocation(tableMetadata.getProperties());
         if (externalLocation != null) {
+            if (!createNonManagedTableEnabled) {
+                throw new PrestoException(NOT_SUPPORTED, "Cannot create non-managed Hive table");
+            }
+
             external = true;
             targetPath = getExternalPath(session.getUser(), externalLocation);
         }

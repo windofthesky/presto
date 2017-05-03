cafa41db4bc605d342d591ea4009386ac30714e8
Alan Post <alan.post@teradata.com>
2017-05-03 09:59:05 -0400

    Adjust to presto style.

--- presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PruneUnreferencedOutputs.java
+++ presto-main/src/main/java/com/facebook/presto/sql/planner/iterative/rule/PruneUnreferencedOutputs.java
@@ -26,13 +26,17 @@ import com.facebook.presto.sql.planner.plan.PlanNode;
 import com.facebook.presto.sql.planner.plan.PlanVisitor;
 import com.facebook.presto.sql.planner.plan.ProjectNode;
 import com.facebook.presto.sql.planner.plan.SimplePlanRewriter;
+import com.facebook.presto.sql.planner.plan.TableScanNode;
 import com.facebook.presto.sql.planner.plan.ValuesNode;
 import com.facebook.presto.sql.tree.Expression;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.UnmodifiableIterator;
 
+import java.util.Arrays;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -45,19 +49,25 @@ public class PruneUnreferencedOutputs
             extends PlanVisitor<Void, ImmutableList<ImmutableSet<Symbol>>>
     {
         @Override
-        public ImmutableList<ImmutableSet<Symbol>> visitPlan(PlanNode projectNode, Void context)
+        public ImmutableList<ImmutableSet<Symbol>> visitPlan(PlanNode node, Void context)
         {
-            throw new RuntimeException("Unexpected plan node type " + projectNode.getClass().getName());
+            throw new RuntimeException("Unexpected plan node type " + node.getClass().getName());
         }
 
         @Override
-        public ImmutableList<ImmutableSet<Symbol>> visitProject(ProjectNode projectNode, Void context)
+        public ImmutableList<ImmutableSet<Symbol>> visitProject(ProjectNode node, Void context)
         {
-            return ImmutableList.of(ImmutableSet.copyOf(DependencyExtractor.extractUnique(projectNode)));
+            return ImmutableList.of(ImmutableSet.copyOf(DependencyExtractor.extractUnique(node)));
         }
 
         @Override
-        public ImmutableList<ImmutableSet<Symbol>> visitValues(ValuesNode valuesNode, Void context)
+        public ImmutableList<ImmutableSet<Symbol>> visitTableScan(TableScanNode node, Void context)
+        {
+            return ImmutableList.of();
+        }
+
+        @Override
+        public ImmutableList<ImmutableSet<Symbol>> visitValues(ValuesNode node, Void context)
         {
             return ImmutableList.of();
         }
@@ -66,33 +76,50 @@ public class PruneUnreferencedOutputs
     private static class RestrictOutputSymbols extends PlanVisitor<ImmutableSet<Symbol>, PlanNode>
     {
         @Override
-        public PlanNode visitPlan(PlanNode projectNode, ImmutableSet<Symbol> requiredSymbols)
+        public PlanNode visitPlan(PlanNode node, ImmutableSet<Symbol> requiredSymbols)
         {
-            throw new RuntimeException("Unexpected plan node type " + projectNode.getClass().getName());
+            throw new RuntimeException("Unexpected plan node type " + node.getClass().getName());
         }
 
         @Override
-        public PlanNode visitProject(ProjectNode projectNode, ImmutableSet<Symbol> requiredSymbols)
+        public PlanNode visitProject(ProjectNode node, ImmutableSet<Symbol> requiredSymbols)
         {
             return new ProjectNode(
-                    projectNode.getId(),
-                    projectNode.getSource(),
-                    projectNode.getAssignments().filter(requiredSymbols));
+                    node.getId(),
+                    node.getSource(),
+                    node.getAssignments().filter(requiredSymbols));
         }
 
         @Override
-        public PlanNode visitValues(ValuesNode valuesNode, ImmutableSet<Symbol> requiredSymbols)
+        public PlanNode visitTableScan(TableScanNode node, ImmutableSet<Symbol> requiredSymbols)
         {
-            final ImmutableList<Integer> projection = IntStream.range(0, valuesNode.getOutputSymbols().size()).filter(
-                    column -> requiredSymbols.contains(valuesNode.getOutputSymbols().get(column))
-            ).boxed().collect(toImmutableList());
+            return new TableScanNode(
+                    node.getId(),
+                    node.getTable(),
+                    ImmutableList.copyOf(requiredSymbols),
+                    Maps.filterKeys(node.getAssignments(), requiredSymbols::contains),
+                    node.getLayout(),
+                    node.getCurrentConstraint(),
+                    node.getOriginalConstraint());
+        }
+
+        @Override
+        public PlanNode visitValues(ValuesNode node, ImmutableSet<Symbol> requiredSymbols)
+        {
+            final int[] projection = IntStream.range(0, node.getOutputSymbols().size())
+                    .filter(column -> requiredSymbols.contains(node.getOutputSymbols().get(column)))
+                    .toArray();
 
             return new ValuesNode(
-                    valuesNode.getId(),
-                    projection.stream().map(valuesNode.getOutputSymbols()::get).collect(toImmutableList()),
-                    valuesNode.getRows().stream().map(
-                            row -> projection.stream().map(row::get).collect(toImmutableList())
-                    ).collect(toImmutableList()));
+                    node.getId(),
+                    Arrays.stream(projection)
+                            .mapToObj(node.getOutputSymbols()::get)
+                            .collect(toImmutableList()),
+                    node.getRows().stream()
+                            .map(row -> Arrays.stream(projection)
+                                    .mapToObj(row::get)
+                                    .collect(toImmutableList()))
+                            .collect(toImmutableList()));
         }
     }
 

5bc859ff1612c8735af055fb98b7044ceb8b9b41
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-05-30 16:37:31 +0200

    Add LimitStatsRule (TODO)

--- presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
+++ presto-main/src/main/java/com/facebook/presto/server/ServerMainModule.java
@@ -28,6 +28,7 @@ import com.facebook.presto.cost.CostCalculatorUsingExchanges;
 import com.facebook.presto.cost.CostCalculatorWithEstimatedExchanges;
 import com.facebook.presto.cost.CostComparator;
 import com.facebook.presto.cost.FilterStatsCalculator;
+import com.facebook.presto.cost.LimitStatsRule;
 import com.facebook.presto.cost.OutputStatsRule;
 import com.facebook.presto.cost.ScalarStatsCalculator;
 import com.facebook.presto.cost.StatsCalculator;
@@ -467,6 +468,7 @@ public class ServerMainModule
         rules.add(new OutputStatsRule());
         rules.add(new TableScanStatsRule(metadata));
         rules.add(new ValuesStatsRule());
+        rules.add(new LimitStatsRule());
 
         return new ComposableStatsCalculator(rules.build());
     }

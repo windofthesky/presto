c5a175490ad25e1052c29c74945db524c09c8016
Lukasz Osipiuk <lukasz@osipiuk.net>
2017-05-30 16:08:12 +0200

    Add ValuesStatsRule (TODO)

---
+++ presto-main/src/main/java/com/facebook/presto/cost/ValuesStatsRule.java
@@ -0,0 +1,42 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.facebook.presto.cost;
+
+import com.facebook.presto.Session;
+import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.sql.planner.iterative.Lookup;
+import com.facebook.presto.sql.planner.plan.PlanNode;
+import com.facebook.presto.sql.planner.plan.ValuesNode;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class ValuesStatsRule
+        implements ComposableStatsCalculator.Rule
+{
+    @Override
+    public Optional<PlanNodeStatsEstimate> calculate(PlanNode node, Lookup lookup, Session session, Map<Symbol, Type> types)
+    {
+        if (!(node instanceof ValuesNode)) {
+            return Optional.empty();
+        }
+        ValuesNode valuesNode = (ValuesNode) node;
+
+        // TODO symbol stats
+        return Optional.of(PlanNodeStatsEstimate.builder()
+                .setOutputRowCount(valuesNode.getRows().size())
+                .build());
+    }
+}

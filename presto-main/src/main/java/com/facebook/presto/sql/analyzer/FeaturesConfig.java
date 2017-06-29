a6e303fa96007e98c162f5aec56108948aa90e7d
Rebecca Schlussel <rebecca.schlussel@teradata.com>
2017-06-29 14:03:36 -0400

    Replace distributed_joins with join_distribution_type session property
    The join_distribution_type property has three option: "repartitioned",
    "replicated", and "automatic". This property replaces the
    "distributed_joins" property. "Repartitioned" has the same behavior as
    distribtued_joins=true. "Replicated" is equivalent to
    distributed_joins=false. "Automatic" will use stats to evaluate whether
    partitioned or replicated is better, or if no information is available,
    it will choose partitioned.
    
    The default value for join_distribution_type is "repartitioned" .

--- presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
+++ presto-main/src/main/java/com/facebook/presto/sql/analyzer/FeaturesConfig.java
@@ -27,8 +27,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
+import static com.facebook.presto.sql.analyzer.FeaturesConfig.JoinDistributionType.REPARTITIONED;
 import static com.facebook.presto.sql.analyzer.RegexLibrary.JONI;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 @DefunctConfig({
@@ -42,8 +44,8 @@ public class FeaturesConfig
     private double cpuCostWeight = 0.75;
     private double memoryCostWeight = 0;
     private double networkCostWeight = 0.25;
+
     private boolean distributedIndexJoinsEnabled;
-    private boolean distributedJoinsEnabled = true;
     private boolean colocatedJoinsEnabled;
     private boolean fastInequalityJoins = true;
     private boolean reorderJoins = true;
@@ -58,6 +60,7 @@ public class FeaturesConfig
     private boolean legacyOrderBy;
     private boolean legacyMapSubscript;
     private boolean optimizeMixedDistinctAggregations;
+    private JoinDistributionType joinDistributionType = REPARTITIONED;
 
     private boolean dictionaryAggregation;
     private boolean resourceGroups;
@@ -75,6 +78,23 @@ public class FeaturesConfig
     private boolean useNewStatsCalculator = false;
 
     private Duration iterativeOptimizerTimeout = new Duration(3, MINUTES); // by default let optimizer wait a long time in case it retrieves some data from ConnectorMetadata
+
+    public enum JoinDistributionType
+    {
+        AUTOMATIC,
+        REPLICATED,
+        REPARTITIONED;
+
+        public boolean canRepartition()
+        {
+            return this == REPARTITIONED || this == AUTOMATIC;
+        }
+
+        public boolean canReplicate()
+        {
+            return this == REPLICATED || this == AUTOMATIC;
+        }
+    }
 
     public double getCpuCostWeight()
     {
@@ -136,11 +156,6 @@ public class FeaturesConfig
         return this;
     }
 
-    public boolean isDistributedJoinsEnabled()
-    {
-        return distributedJoinsEnabled;
-    }
-
     @Config("deprecated.legacy-array-agg")
     public FeaturesConfig setLegacyArrayAgg(boolean legacyArrayAgg)
     {
@@ -175,13 +190,6 @@ public class FeaturesConfig
     public boolean isLegacyMapSubscript()
     {
         return legacyMapSubscript;
-    }
-
-    @Config("distributed-joins-enabled")
-    public FeaturesConfig setDistributedJoinsEnabled(boolean distributedJoinsEnabled)
-    {
-        this.distributedJoinsEnabled = distributedJoinsEnabled;
-        return this;
     }
 
     public boolean isColocatedJoinsEnabled()
@@ -476,5 +484,17 @@ public class FeaturesConfig
     public boolean isUseNewStatsCalculator()
     {
         return useNewStatsCalculator;
+    }
+
+    @Config("join-distribution-type")
+    public FeaturesConfig setJoinDistributionType(JoinDistributionType joinDistributionType)
+    {
+        this.joinDistributionType = requireNonNull(joinDistributionType, "joinDistributionType is null");
+        return this;
+    }
+
+    public JoinDistributionType getJoinDistributionType()
+    {
+        return joinDistributionType;
     }
 }

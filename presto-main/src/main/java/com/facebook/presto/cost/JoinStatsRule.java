4edecee079656b276a50213bd2191d4e4793aee8
Grzegorz Kokosiński <g.kokosinski@gmail.com>
2017-07-06 12:23:13 +0200

    Revert "fixup! Add JoinStatsRule to support equi-conditions and filters"
    This reverts commit 42464392051dc60f6f4444e3238cd30ed700cdac.

--- presto-main/src/main/java/com/facebook/presto/cost/JoinStatsRule.java
+++ presto-main/src/main/java/com/facebook/presto/cost/JoinStatsRule.java
@@ -127,9 +127,8 @@ public class JoinStatsRule
             SymbolStatsEstimate leftColumnStats = leftStats.getSymbolStatistics(clause.getLeft());
             SymbolStatsEstimate rightColumnStats = rightStats.getSymbolStatistics(clause.getRight());
 
-            StatisticRange rightRange = StatisticRange.from(rightColumnStats);
-            StatisticRange antiRange = StatisticRange.from(leftColumnStats)
-                    .subtract(rightRange);
+            StatisticRange rightRange = new StatisticRange(rightColumnStats.getLowValue(), rightColumnStats.getHighValue(), rightColumnStats.getDistinctValuesCount());
+            StatisticRange antiRange = new StatisticRange(leftColumnStats.getLowValue(), leftColumnStats.getHighValue(), leftColumnStats.getDistinctValuesCount()).subtract(rightRange);
 
             // TODO: use NDVs from left and right StatisticRange when they are fixed
             double leftNDV = leftColumnStats.getDistinctValuesCount();

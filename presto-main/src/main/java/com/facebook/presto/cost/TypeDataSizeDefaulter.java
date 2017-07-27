29130be71b4f3b5b0ed3990ba4c641e38ec44e39
Piotr Findeisen <piotr.findeisen@teradata.com>
2017-07-28 00:06:21 +0200

    Estimate data size based on data type

--- presto-main/src/main/java/com/facebook/presto/cost/TypeDataSizeDefaulter.java
+++ presto-main/src/main/java/com/facebook/presto/cost/TypeDataSizeDefaulter.java
@@ -13,8 +13,20 @@
  */
 package com.facebook.presto.cost;
 
+import com.facebook.presto.spi.type.ArrayType;
+import com.facebook.presto.spi.type.CharType;
+import com.facebook.presto.spi.type.FixedWidthType;
+import com.facebook.presto.spi.type.MapType;
+import com.facebook.presto.spi.type.RowType;
 import com.facebook.presto.spi.type.Type;
+import com.facebook.presto.spi.type.VarbinaryType;
+import com.facebook.presto.spi.type.VarcharType;
 import com.facebook.presto.sql.planner.Symbol;
+import com.facebook.presto.type.JoniRegexpType;
+import com.facebook.presto.type.JsonType;
+import com.facebook.presto.type.LikePatternType;
+import com.facebook.presto.type.ListLiteralType;
+import com.facebook.presto.type.Re2JRegexpType;
 
 import javax.annotation.concurrent.ThreadSafe;
 
@@ -27,11 +39,30 @@ import static java.util.Objects.requireNonNull;
 @ThreadSafe
 public class TypeDataSizeDefaulter
 {
-    private static final double DEFAULT_DATA_SIZE_PER_COLUMN = 10;
+    private static final double DEFAULT_VARCHAR_DATA_SIZE_PER_COLUMN = 128;
+    private static final double DEFAULT_COLLECTION_DATA_SIZE_PER_COLUMN = 256;
+    private static final double DEFAULT_PATTERN_DATA_SIZE_PER_COLUMN = 32;
+    private static final double DEFAULT_DATA_SIZE_PER_COLUMN = 50;
 
     public double defaultDataSize(Type type)
     {
         requireNonNull(type, "type is null");
+
+        if (type instanceof FixedWidthType) {
+            return ((FixedWidthType) type).getFixedSize();
+        }
+
+        if (type instanceof VarcharType || type instanceof VarbinaryType || type instanceof CharType) {
+            return DEFAULT_VARCHAR_DATA_SIZE_PER_COLUMN;
+        }
+
+        if (type instanceof MapType || type instanceof RowType || type instanceof ArrayType || type instanceof ListLiteralType || type instanceof JsonType) {
+            return DEFAULT_COLLECTION_DATA_SIZE_PER_COLUMN;
+        }
+
+        if (type instanceof JoniRegexpType || type instanceof Re2JRegexpType || type instanceof LikePatternType) {
+            return DEFAULT_PATTERN_DATA_SIZE_PER_COLUMN;
+        }
 
         return DEFAULT_DATA_SIZE_PER_COLUMN;
     }

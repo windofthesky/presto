46cff3190b8437273e5137fc936323aa5f8f267e
Szymon Matejczyk <szymon.matejczyk@teradata.com>
2017-07-31 17:08:18 +0200

    Add test for tcp-h:q3 join order

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
@@ -92,6 +92,32 @@ public class TestReorderJoins
                         new TableScan("tpch:lineitem:sf10.0")));
     }
 
+    @Test
+    public void testTpchQ3JoinOrder()
+    {
+        assertJoinOrder(
+                "SELECT " +
+                        "l.orderkey, " +
+                        "sum(l.extendedprice * (1 - l.discount)) AS revenue, " +
+                        "o.orderdate, o.shippriority " +
+                        "FROM customer AS c, orders AS o, lineitem AS l " +
+                        "WHERE c.mktsegment = 'BUILDING' " +
+                        "AND c.custkey = o.custkey " +
+                        "AND l.orderkey = o.orderkey " +
+                        "AND o.orderdate<DATE '1995-03-15' " +
+                        "AND l.shipdate > DATE '1995-03-15' " +
+                        "GROUP BY l.orderkey, o.orderdate, o.shippriority " +
+                        "ORDER BY revenue DESC, o.orderdate LIMIT 10",
+                new Join(INNER,
+                         REPLICATED,
+                         new TableScan("tpch:lineitem:sf10.0"),
+                         new Join(
+                                 INNER,
+                                 REPLICATED,
+                                 new TableScan("tpch:orders:sf10.0"),
+                                 new TableScan("tpch:customer:sf10.0"))));
+    }
+
     private void assertJoinOrder(String sql, Node expected)
     {
         assertEquals(joinOrderString(sql), expected.print());

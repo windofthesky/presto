d04cd915e876422b4517bdb85897c2ee89d3c08b
Grzegorz Kokosiński <Grzegorz.Kokosinski@Teradata.com>
2017-08-01 08:01:55 +0200

    Add q05

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
@@ -92,9 +92,9 @@ public class TestReorderJoins
                         "ORDER BY revenue DESC, o.orderdate LIMIT 10",
                 new Join(
                         tpchSf10Table("lineitem"),
-                         new Join(
-                                 tpchSf10Table("orders"),
-                                 tpchSf10Table("customer"))));
+                        new Join(
+                                tpchSf10Table("orders"),
+                                tpchSf10Table("customer"))));
     }
 
     @Test
@@ -120,6 +120,48 @@ public class TestReorderJoins
                         Optional.empty(),
                         tpchSf10Table("orders"),
                         tpchSf10Table("lineitem")));
+    }
+
+    @Test
+    public void testTpchQ05JoinOrder()
+    {
+        assertJoinOrder(
+                "" +
+                        "SELECT " +
+                        "  n.name, " +
+                        "  sum(l.extendedprice * (1 - l.discount)) AS revenue " +
+                        "FROM " +
+                        "  customer AS c, " +
+                        "  orders AS o, " +
+                        "  lineitem AS l, " +
+                        "  supplier AS s, " +
+                        "  nation AS n, " +
+                        "  region AS r " +
+                        "WHERE " +
+                        "  c.custkey = o.custkey " +
+                        "  AND l.orderkey = o.orderkey " +
+                        "  AND l.suppkey = s.suppkey " +
+                        "  AND c.nationkey = s.nationkey " +
+                        "  AND s.nationkey = n.nationkey " +
+                        "  AND n.regionkey = r.regionkey " +
+                        "  AND r.name = 'ASIA' " +
+                        "  AND o.orderdate >= DATE '1994-01-01' " +
+                        "  AND o.orderdate < DATE '1994-01-01' + INTERVAL '1' YEAR " +
+                        "GROUP BY " +
+                        "  n.name " +
+                        "ORDER BY " +
+                        "  revenue DESC ",
+                new Join(
+                        new Join(
+                                tpchSf10Table("lineitem"),
+                                new Join(
+                                        tpchSf10Table("orders"),
+                                        new Join(
+                                                tpchSf10Table("customer"),
+                                                new Join(
+                                                        tpchSf10Table("nation"),
+                                                        tpchSf10Table("region"))))),
+                        tpchSf10Table("supplier")));
     }
 
     @Test
@@ -224,7 +266,8 @@ public class TestReorderJoins
             this(REPLICATED, left, right);
         }
 
-        private Join(JoinNode.DistributionType distributionType, Node left, Node right) {
+        private Join(JoinNode.DistributionType distributionType, Node left, Node right)
+        {
             this(INNER, Optional.of(distributionType), left, right);
         }
 

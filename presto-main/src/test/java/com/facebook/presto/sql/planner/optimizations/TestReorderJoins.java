0b88512b6cdaca642b001e5547152ebcc3037cd1
Szymon Matejczyk <szymon.matejczyk@teradata.com>
2017-08-01 11:21:15 +0200

    Add test Tpch q7 join order

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
@@ -169,6 +169,47 @@ public class TestReorderJoins
     // no joins in q6
 
     @Test
+    public void testInnerTpchQ7JoinOrder()
+    {
+        assertJoinOrder(
+                "SELECT " +
+                        "n1.name AS supp_nation, " +
+                        "n2.name AS cust_nation, " +
+                        "extract(YEAR FROM l.shipdate) AS l_year, " +
+                        "l.extendedprice * (1 - l.discount) AS volume " +
+                        "FROM " +
+                        "supplier AS s, " +
+                        "lineitem AS l, " +
+                        "orders AS o, " +
+                        "customer AS c, " +
+                        "nation AS n1, " +
+                        "nation AS n2 " +
+                        "WHERE " +
+                        "s.suppkey = l.suppkey " +
+                        "AND o.orderkey = l.orderkey " +
+                        "AND c.custkey = o.custkey " +
+                        "AND s.nationkey = n1.nationkey " +
+                        "AND c.nationkey = n2.nationkey " +
+                        "AND ( " +
+                        "        (n1.name = 'FRANCE' AND n2.name = 'GERMANY')" +
+                        "OR (n1.name = 'GERMANY' AND n2.name = 'FRANCE') " +
+                        ") " +
+                        "AND l.shipdate BETWEEN DATE '1995-01-01' AND DATE '1996-12-31'",
+                new Join(
+                        new Join(
+                                tpchSf10Table("lineitem"),
+                                new Join(
+                                        tpchSf10Table("orders"),
+                                        new Join(
+                                                tpchSf10Table("customer"),
+                                                tpchSf10Table("nation")))),
+                        new Join(
+                                tpchSf10Table("supplier"),
+                                tpchSf10Table("nation"))));
+    }
+
+
+    @Test
     public void testPartialTpchQ14JoinOrder()
     {
         // it looks like the join ordering here is optimal

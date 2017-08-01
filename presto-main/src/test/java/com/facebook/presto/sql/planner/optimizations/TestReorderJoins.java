f60255a93d60e6a6631e89f3d9004b3131e3ff87
Szymon Matejczyk <szymon.matejczyk@teradata.com>
2017-08-01 11:32:37 +0200

    Add test Tpch Q8 join order

--- presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
+++ presto-main/src/test/java/com/facebook/presto/sql/planner/optimizations/TestReorderJoins.java
@@ -208,6 +208,50 @@ public class TestReorderJoins
                                 tpchSf10Table("nation"))));
     }
 
+    @Test
+    public void testInnerTpchQ8JoinOrder()
+    {
+        assertJoinOrder(
+                "SELECT " +
+                        "extract(YEAR FROM o.orderdate)AS o_year, " +
+                        "l.extendedprice * (1 - l.discount) AS volume, " +
+                        "n2.name AS nation " +
+                        "FROM " +
+                        "part AS p, " +
+                        "supplier AS s, " +
+                        "lineitem AS l, " +
+                        "orders AS o, " +
+                        "customer AS c, " +
+                        "nation AS n1, " +
+                        "nation AS n2, " +
+                        "region AS r " +
+                        "WHERE " +
+                        "p.partkey = l.partkey " +
+                        "AND s.suppkey = l.suppkey " +
+                        "AND l.orderkey = o.orderkey " +
+                        "AND o.custkey = c.custkey " +
+                        "AND c.nationkey = n1.nationkey " +
+                        "AND n1.regionkey = r.regionkey " +
+                        "AND r.name = 'AMERICA' " +
+                        "AND s.nationkey = n2.nationkey " +
+                        "AND o.orderdate BETWEEN DATE '1995-01-01' AND DATE '1996-12-31' " +
+                        "AND p.type = 'ECONOMY ANODIZED STEEL'",
+                new Join(
+                        new Join(
+                                new Join(
+                                        tpchSf10Table("orders"),
+                                        new Join(
+                                                tpchSf10Table("lineitem"),
+                                                tpchSf10Table("part"))),
+                                new Join(
+                                        tpchSf10Table("customer"),
+                                        new Join(
+                                                tpchSf10Table("nation"),
+                                                tpchSf10Table("region")))),
+                        new Join(
+                                tpchSf10Table("supplier"),
+                                tpchSf10Table("nation"))));
+    }
 
     @Test
     public void testPartialTpchQ14JoinOrder()

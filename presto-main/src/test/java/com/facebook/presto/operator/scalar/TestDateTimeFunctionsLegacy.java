51d426678d82360511f34b7b4e47e0d7127b8123
Andrzej Fiedukowicz <Andrzej.Fiedukowicz@teradata.com>
2017-08-10 18:39:47 +0200

    fixup! Split TestDateTimeFunctions into variants based on isLegacyTimestamp

--- presto-main/src/test/java/com/facebook/presto/operator/scalar/TestDateTimeFunctionsLegacy.java
+++ presto-main/src/test/java/com/facebook/presto/operator/scalar/TestDateTimeFunctionsLegacy.java
@@ -37,8 +37,7 @@ public class TestDateTimeFunctionsLegacy
                         .setTimeZoneKey(TIME_ZONE_KEY)
                         .setSystemProperty("legacy_timestamp", "true")
                         .setStartTime(new DateTime(2017, 04, 01, 12, 34, 56, 789, UTC_TIME_ZONE).getMillis())
-                        .build()
-        );
+                        .build());
     }
 
     @Test
